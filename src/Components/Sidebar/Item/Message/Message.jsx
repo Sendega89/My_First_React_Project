f17c052fc1be25bcb890c7f7a94b6cd91e3fe3b6
Sendega89 <78374273+Sendega89@users.noreply.github.com>
2021-09-17 12:33:53 +0300

    add Navlink to Message.jsx

--- src/Components/Sidebar/Item/Message/Message.jsx
+++ src/Components/Sidebar/Item/Message/Message.jsx
@@ -1,10 +1,36 @@
 import React from "react";
 import s from "./Message.module.css";
+import {NavLink} from "react-router-dom";
 
 function Message() {
     return (
-        <div className={s.item}>
-           Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogs_item}>
+                <div className={s.dialog}>
+                    <NavLink to="/Message/dialogs/1">Andrey </NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/Message/dialogs/2"> Gleb</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/Message/dialogs/3"> Alosha</NavLink>
+                </div>
+                <div className={s.dialog + ' ' + s.active}>
+                    <NavLink to="/Message/dialogs/4"> Misha</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/Message/dialogs/5"> Vitaxa</NavLink>
+                </div>
+
+
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>Ho</div>
+                <div className={s.message}>Hy</div>
+                <div className={s.message}>He</div>
+                <div className={s.message}>Ha</div>
+            </div>
         </div>
     )
 }
