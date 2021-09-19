22256987bc4bf48f7e767364974c7bc2e1c80143
Sendega89 <78374273+Sendega89@users.noreply.github.com>
2021-09-19 23:55:10 +0300

    refactoring block Profile.jsx add massives dialogsData,messageData, refactoring Message.jsx

--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -3,16 +3,22 @@ import s from './MyPosts.module.css';
 import Post from "./Post/Post";
 
 function MyPosts() {
+    let postData = [
+        {id: 1, message: "Hi,how are you", likesCount: '23'},
+        {id: 2, message: 'It is my first post', likesCount: '22'},
+    ]
     return (
         <div className={s.myPost}>
             <h1>My posts</h1>
             <div className={s.textArea}>
-                <textarea></textarea>
+                <textarea/>
+            </div>
+            <div>
                 <button className={s.button}>Add post</button>
             </div>
             <div className={s.posts}>
-                <Post value='Hi,how are you?' likesCount='0'/>
-                <Post value='It is my first post' likesCount='22'/>
+                <Post message={postData[0].message} likesCount={postData[0].likesCount}/>
+                <Post message={postData[1].message} likesCount={postData[1].likesCount}/>
 
             </div>
         </div>
--- src/Components/Sidebar/Item/Message/Message.jsx
+++ src/Components/Sidebar/Item/Message/Message.jsx
@@ -18,21 +18,35 @@ function MessageContent(props) {
 }
 
 function Message(props) {
+    let dialogsData = [
+        {id: 1, name: 'Andrey'},
+        {id: 2, name: 'Aloha'},
+        {id: 3, name: 'Misha'},
+        {id: 4, name: 'Gleb'},
+        {id: 5, name: 'Tima'},
+    ]
+    let messageData = [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'Ho'},
+        {id: 3, message: 'He'},
+        {id: 4, message: 'Hy'},
+        {id: 5, message: 'Yo'},
+    ]
     return (
         <div className={s.dialogs}>
             <div className={s.dialogs_item}>
-                <MessageItem name="Andrey" id="1"/>
-                <MessageItem name="Aloha" id="2"/>
-                <MessageItem name="Misha" id="3"/>
-                <MessageItem name="Gleb" id="4"/>
-                <MessageItem name="Tima" id="5"/>
+                <MessageItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                <MessageItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+                <MessageItem name={dialogsData[2].name} id={dialogsData[2].id}/>
+                <MessageItem name={dialogsData[3].name} id={dialogsData[3].id}/>
+                <MessageItem name={dialogsData[4].name} id={dialogsData[4].id}/>
             </div>
             <div className={s.messages}>
-                <MessageContent message="Hi"/>
-                <MessageContent message="He"/>
-                <MessageContent message="Ho"/>
-                <MessageContent message="Hy"/>
-                <MessageContent message="Yo"/>
+                <MessageContent message={messageData[0].message}/>
+                <MessageContent message={messageData[1].message}/>
+                <MessageContent message={messageData[2].message}/>
+                <MessageContent message={messageData[3].message}/>
+                <MessageContent message={messageData[4].message}/>
 
             </div>
         </div>
