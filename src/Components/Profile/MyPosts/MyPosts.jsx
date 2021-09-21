054974653d69818a96ec73e87c59670e3583a57a
Sendega89 <78374273+Sendega89@users.noreply.github.com>
2021-09-21 22:30:30 +0300

    refactoring block Profile.jsx add massives dialogsData,messageData, refactoring Message.jsx add dialogsElements add messageElements fix messageElements

--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -4,9 +4,10 @@ import Post from "./Post/Post";
 
 function MyPosts() {
     let postData = [
-        {id: 1, message: "Hi,how are you", likesCount: '23'},
+        {id: 1, message: "Hi,how are you", likesCount: '222'},
         {id: 2, message: 'It is my first post', likesCount: '22'},
     ]
+    let posts = postData.map(post => <Post message={post.message} likesCount={post.likesCount}/>)
     return (
         <div className={s.myPost}>
             <h1>My posts</h1>
@@ -17,8 +18,8 @@ function MyPosts() {
                 <button className={s.button}>Add post</button>
             </div>
             <div className={s.posts}>
-                <Post message={postData[0].message} likesCount={postData[0].likesCount}/>
-                <Post message={postData[1].message} likesCount={postData[1].likesCount}/>
+                {posts}
+
 
             </div>
         </div>
