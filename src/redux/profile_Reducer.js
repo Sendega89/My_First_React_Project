e4dd1bad6e40537c72d310eed309ca6811513eca
Sendega89 <78374273+Sendega89@users.noreply.github.com>
2021-10-29 19:48:36 +0300

    refactoring dialogs_Reducer.js refactoring profile_Reducer.js

--- src/redux/profile_Reducer.js
+++ src/redux/profile_Reducer.js
@@ -3,34 +3,35 @@ const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
 
 let initialState = {
-            postData: [
-                {id: 1, message: "Hi,how are you", likesCount: '2'},
-                {id: 2, message: 'It is my first post', likesCount: '4'},
-                {id: 3, message: 'It is my second post', likesCount: '23'},
-                {id: 4, message: 'It is my last post', likesCount: '8'},
-            ],
-            newPostText: 'Enter text here'
+    postData: [
+        {id: 1, message: "Hi,how are you", likesCount: '2'},
+        {id: 2, message: 'It is my first post', likesCount: '4'},
+        {id: 3, message: 'It is my second post', likesCount: '23'},
+        {id: 4, message: 'It is my last post', likesCount: '8'},
+    ],
+    newPostText: 'Enter text here'
 }
 const profile_Reducer = (state = initialState, action) => {
-    let stateCopy = {...state};
+
 
     switch (action.type) {
-        case ADD_POST: {
+        case ADD_POST:
             let newPost = {
                 id: 5,
                 message: state.newPostText,
                 likesCount: '1'
             };
-            stateCopy.postData = [...state.postData];
-            stateCopy.postData.push(newPost);
-            stateCopy.newPostText = '';
-            return stateCopy;
-        }
-        case UPDATE_NEW_POST_TEXT:{
-            stateCopy.postData = [...state.postData]
-            stateCopy.newPostText = action.newText;
-            return stateCopy;
-    }
+            return {
+                ...state,
+                postData: [...state.postData, newPost],
+                newPostText: ''
+            };
+        case UPDATE_NEW_POST_TEXT:
+            return {
+                ...state,
+                newPostText: action.newText
+            }
+
         default:
             return state;
     }
