98eaac1406342c58058ce1fe2e9d2ad4c42c8d7b
Sendega89 <78374273+Sendega89@users.noreply.github.com>
2021-12-08 16:33:03 +0200

    addCompose

--- src/Components/Profile/ProfileStatus.jsx
+++ src/Components/Profile/ProfileStatus.jsx
@@ -3,25 +3,34 @@ import s from "./Profile.module.css";
 
 
 class ProfileStatus extends React.Component {
-state = {
-    editMode: false
-}
-activateEditMode = () => {
-this.state.editMode = true;
+    state = {
+        editMode: false
+    }
+    activateEditMode = () => {
+        this.setState({
+                editMode: true
+            }
+        )
+    }
+    deActivateEditMode = () => {
+        this.setState({
+                editMode: false
+            }
+        )
+    }
 
-}
     render() {
         return (
             <div className={s.statusContainer}>
                 <div className={s.statusBox}>
                     {!this.state.editMode &&
-                    <div className={s.statusInfo}>
-                        <span onDoubleClick={this.activateEditMode}>{this.props.status}</span>
-                    </div>}
+                        <div className={s.statusInfo}>
+                            <span onDoubleClick={this.activateEditMode.bind(this)}>{this.props.status}</span>
+                        </div>}
                     {this.state.editMode &&
-                    <div className={s.statusInput}>
-                        <input value={this.props.status}/>
-                    </div>}
+                        <div className={s.statusInput}>
+                            <input autoFocus={true} onBlur={this.deActivateEditMode.bind(this)} value={this.props.status}/>
+                        </div>}
                 </div>
 
             </div>
