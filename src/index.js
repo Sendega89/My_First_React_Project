0fc9db4e29e21ff1bdf0af7ef9abd1a3a00d905a
Sendega89 <78374273+Sendega89@users.noreply.github.com>
2022-03-18 13:12:13 +0200

    add HashRouter

--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import ReactDOM from "react-dom";
 import React from "react";
 import './index.css';
 import App from "./App";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter basename={process.env.PUBLIC_URL}>
             <Provider store={store}>
                 <App/>
             </Provider>
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>,
     document.getElementById('root')
 );
